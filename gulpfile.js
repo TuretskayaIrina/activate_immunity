83159014241ecdc78d4c983c8a8cece3aa604598
Irina Yakovleva <turetskaya30.07.1994@gmail.com>
2020-10-14 18:07:42 +0300

    test gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/* Определяем переменные */
+
+const gulp = require('gulp'), // Сообственно Gulp JS
+const uglify = require('gulp-uglify'), // Минификация JS
+const concat = require('gulp-concat'), // Склейка файлов
+const imagemin = require('gulp-imagemin'), // Минификация изображений
+const csso = require('gulp-csso'), // Минификация CSS
+const sass = require('gulp-sass'); // Конверстация SASS (SCSS) в CSS
